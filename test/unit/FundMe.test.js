c07d8285097900616c5005523c4261cb257803c9
Pratyush Sapkota <pratyushsapkota@gmail.com>
2023-06-28 11:17:03 +0545

    added one more test

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -3,17 +3,18 @@ const { ethers } = require("hardhat")
 
 describe("FundMe", async function () {
     let fundMe
-    let deployer
     let mockV3Aggregator
+    const sendValue = 1 * 10 ** 18
+    let deployerAddress
 
     beforeEach(async function () {
         mockV3Aggregator = await ethers.deployContract("MockV3Aggregator", [18, 0])
-        await mockV3Aggregator.waitForDeployment();
-
+        await mockV3Aggregator.waitForDeployment()
         fundMe = await ethers.deployContract("FundMe", [mockV3Aggregator.target])
-        await fundMe.waitForDeployment(); 
-
-
+        await fundMe.waitForDeployment()
+        let [signer] = await ethers.getSigner()
+        deployerAddress = signer.address
+        
     })
 
     describe("Constructor", async function () {
@@ -24,9 +25,16 @@ describe("FundMe", async function () {
         })
     })
 
-    describe("Fund", async function() {
-        it("Fails if not enough eth", async function() {
-            await expect (fundMe.fund()).to.be.revertedWith("Need more eth!")
+    describe("Fund", async function () {
+        it("Fails if not enough eth", async function () {
+            expect(fundMe.fund())
+        })
+
+        it("Updates the amount funded mapping thing", async function () {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressToAmountFunded(deployerAddress)
+
+            assert.equal(response.toString(), sendValue.toString())
         })
     })
 
