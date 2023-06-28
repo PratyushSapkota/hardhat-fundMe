731295c0a2587bd53e8f995d615066c596fe5821
warproxxx <n00bdan13@gmail.com>
2023-06-27 20:41:35 -0700

    New hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,30 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-require("hardhat-deploy");
 
 
 module.exports = {
   solidity: "0.8.18",
   networks: {
 
-    sepolia: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY1],
-      chainId: 11155111,
-      blockConformation: 6,
-    }
+   
 
-  },
-  defaultNetwork: "hardhat",
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    user: {
-      default: 1,
-    }
   }
+
 };
--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,6 +1,5 @@
 const { assert, expect } = require("chai")
-const { deployments, getNamedAccounts} = require("hardhat")
-const { ethers } = require("@nomicfoundation/hardhat-ethers")
+const { ethers } = require("hardhat")
 
 describe("FundMe", async function () {
     let fundMe
@@ -8,17 +7,19 @@ describe("FundMe", async function () {
     let mockV3Aggregator
 
     beforeEach(async function () {
-        deployer = (await getNamedAccounts()).deployer
-        await deployments.fixture(['all'])
-        fundMe = await ethers.getContract("FundMe", deployer)
-        mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer)
+        mockV3Aggregator = await ethers.deployContract("MockV3Aggregator", [18, 0])
+        await mockV3Aggregator.waitForDeployment();
+
+        fundMe = await ethers.deployContract("FundMe", [mockV3Aggregator.target])
+        await fundMe.waitForDeployment(); 
+
 
     })
 
     describe("Constructor", async function () {
         it("Sets the aggregator address correctly", async function () {
             const response = await fundMe.priceFeed()
-            assert.equal(response, mockV3Aggregator.address)
+            assert.equal(response, mockV3Aggregator.target)
 
         })
     })
